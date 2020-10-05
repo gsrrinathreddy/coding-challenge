24872686d260610b9c875b757f826dd3e768db2a
gsrrinathreddy <gsrrinathreddy@gmail.com>
2020-10-05 08:48:32 -0700

    react-router implementation

--- react-client/src/pages/Categories.js
+++ react-client/src/pages/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Categories = () => {
     return(
         <div>
-            Hi this is a categories page
+            Hi this is a categories page using router
         </div>
     )
 }
